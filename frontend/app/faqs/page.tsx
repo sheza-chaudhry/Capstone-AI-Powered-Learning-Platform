091c6232b47f651bb1dcf11e147a70e826ff457d
Sarah Simmons <56512599+SarahSimmmons@users.noreply.github.com>
2026-02-25 19:41:57 -0500

    updated styling of FAQ page

--- frontend/app/faqs/page.tsx
+++ frontend/app/faqs/page.tsx
@@ -12,15 +12,17 @@ export default function FAQs() {
     
 
       <div className=" flex items-center justify-center min-h-screen bg-[#F5E6D3] p-8 ">
-      <div className=" w-full max-w-5xl h-[90vh] border-4 border-black bg-[#E8F4F8] rounded-lg flex flex-col">
-        <div className="flex justify-between items-center p-6">
-      < Link href = "/" className=" back-btn px-8 py-3 bg-[#A8D5E2] text-black font-semibold 
-          rounded-full border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,0.3)] 
-          hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,0.3)] cursor-pointer transition-all">Back</Link>
+      <div className="items-center gap-12 w-full max-w-5xl h-[90vh] border-4 border-black bg-[#E8F4F8] rounded-lg flex flex-col">
+        <div className="w-full flex justify-between items-center p-6">
+          <div className="w-full flex justify-start">
+            < Link href = "/" className=" back-btn px-8 py-3 bg-[#A8D5E2] text-black font-semibold 
+            rounded-full border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,0.3)] 
+            hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,0.3)] cursor-pointer transition-all">Back to Math Tutor</Link>
+          </div>
            </div>
-           <h1 className= "flex flex-col items center gap-8 text-lg font-bold">Frequently Asked Questions</h1>
+           <h1 className= "flex flex-col items-center gap-6 text-lg font-bold">Frequently Asked Questions</h1>
         {faqs.map((faq, index) => (
-            <div key={index} className="faq-item p-6 flex flex-col items-center justify-center gap-3 w-12 h-12 rounded-full border-2 border-black bg-white
+            <div key={index} className="faq-item p-6 flex flex-col items-center justify-center gap-6 w-200 h-12 rounded-full border-2 border-black bg-white
              flex items-center justify-center text-2xl hover:bg-gray-50 transition-colors">
               <p>{faq}</p>  
             </div>
