5093422e35ec3e15a1131599071add7bd1039091
Sarah Simmons <56512599+SarahSimmmons@users.noreply.github.com>
2026-04-05 16:05:40 -0400

    Added Register API Call to SignupForm

--- frontend/components/SignupForm.jsx
+++ frontend/components/SignupForm.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
 export default function SignupForm({ onSuccess, switchToLogin }) {
-  const [email, setEmail] = useState("");
+//   const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
 
     // Basic validation
-    if (!email || !password || !confirmPassword) {
-      alert("Please fill in all fields");
-      return;
+    if (!username || !password || !confirmPassword) {
+        alert("Please fill in all fields");
+        return;
     }
 
     if (password !== confirmPassword) {
@@ -19,21 +20,47 @@ export default function SignupForm({ onSuccess, switchToLogin }) {
       return;
     }
 
-    // Simulate signup -> Needs to be replaced with api call
-    console.log("Creating account:", { email, password });
+    // Signup API Call
+    try {
+        const response = await fetch("http://localhost:8000/auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+        });
 
-    // Close modal or continue flow
-    onSuccess();
+        if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Backend error:", errorData);
+        throw new Error(errorData.detail || "Signup failed");
+        }
+
+        const data = await response.json();
+
+        console.log("User created:", data);
+
+        // TODO: Add error handling for when signup fails
+        // that is visible to the user
+        onSuccess(); // close modal
+
+    } catch (error) {
+        console.error("Signup error:", error);
+        alert("Failed to create account");
+    }
   };
 
   return (
     <form onSubmit={handleSubmit} className="space-y-3">
       <input
-        type="email"
-        placeholder="Email"
+        type="text"
+        placeholder="Username"
         className="w-full border p-2 rounded"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
 
       <input
