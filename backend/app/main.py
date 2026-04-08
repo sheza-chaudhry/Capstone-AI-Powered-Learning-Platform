a6f9892b3eb6603813a4ca98e767cd959d00a3f3
Awo Ali <awoali@Awos-MacBook-Air.local>
2026-04-08 17:40:57 -0400

    created a landing page, fixed the login/signup pages, changed the overall look of the bot.

--- backend/app/main.py
+++ backend/app/main.py
@@ -94,7 +94,12 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
